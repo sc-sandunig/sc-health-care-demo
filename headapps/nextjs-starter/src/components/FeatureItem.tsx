cbeda9364c2977e3c5666bf1021b64823aad6d98
sc-sandunig <sdgg@sitecore.net>
2025-07-07 13:18:11 +0530

    Formatting

--- headapps/nextjs-starter/src/components/FeatureItem.tsx
+++ headapps/nextjs-starter/src/components/FeatureItem.tsx
@@ -19,11 +19,21 @@ export const Default = (props: FeatureItemProps): JSX.Element => {
   return (
     <div className="flex items-start gap-4 mb-10">
       <div className="flex-shrink-0 w-20 h-20 flex self-top md:self-center  items-center justify-center border-3 border-foreground dark:border-background rounded-lg">
-        <JssImage field={props.fields.FeatureIconLight} width={60} height={60} className="block dark:hidden"/>
-        <JssImage field={props.fields.FeatureIconDark} width={60} height={60} className="hidden dark:block"/>
+        <JssImage
+          field={props.fields.FeatureIconLight}
+          width={60}
+          height={60}
+          className="block dark:hidden"
+        />
+        <JssImage
+          field={props.fields.FeatureIconDark}
+          width={60}
+          height={60}
+          className="hidden dark:block"
+        />
       </div>
       <div className="flex-1">
-        <JssRichText field={props.fields.FeatureText} className="text-base"/>
+        <JssRichText field={props.fields.FeatureText} className="text-base" />
       </div>
     </div>
   );
